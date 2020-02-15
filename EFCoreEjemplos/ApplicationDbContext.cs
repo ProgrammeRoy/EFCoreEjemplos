c6f94254ce8ec7e5b62de13e0f4d9355daf0a953
D'artagnan <roymartinezramirez@gmail.com>
2020-02-14 20:02:32 -0500

    Add project files.

---
+++ EFCoreEjemplos/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCoreEjemplos
+{
+  class ApplicationDbContext: DbContext
+  {
+  }
+}
---
+++ EFCoreEjemplos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EFCoreEjemplos
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
