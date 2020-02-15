e2ef712e2a5fd2d1f24b862dd839e663afe6c6a3
D'artagnan <roymartinezramirez@gmail.com>
2020-02-14 20:21:13 -0500

    Specific DataBase Sql Server

--- EFCoreEjemplos/ApplicationDbContext.cs
+++ EFCoreEjemplos/ApplicationDbContext.cs
@@ -7,5 +7,9 @@ namespace EFCoreEjemplos
 {
   class ApplicationDbContext: DbContext
   {
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseSqlServer("Data Source = SQLSERVER; Initial Catalog=EFCore; Integrated Security=True");
+    }
   }
 }
